fc0b100aa9b75f2b80e59b6fb89e8a4aba10e4c4
Douaa <Douaamami@gmail.com>
2019-03-29 11:34:00 +0100

    calcul

---
+++ JS/ctrl.js
@@ -0,0 +1,9 @@
+function message(){
+    alert('bonjour a tous!!');
+};
+function message2(t){
+    alert('bonjour! '+t.value);
+}
+function carre(tNb){
+    return tNb.value*tNb.value;
+}
--- JS/test.js
+++ JS/test.js
@@ -1,1 +1,6 @@
 alert('hello world');
+var x=2;
+document.write('type de x est '+typeof(x));
+var b="5";
+document.write(b+1);
+document.write(<br/>);
